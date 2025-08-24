31c3066db5a8cd2db6014f4e289fc89298e79f5c
ashrithasgit <159109155+ashrithasgit@users.noreply.github.com>
2025-08-24 19:26:26 +0530

    Create mainscene.cs

---
+++ mainscene.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class intomain : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1); // Use the name of your gameplay scene
+    }
+}
